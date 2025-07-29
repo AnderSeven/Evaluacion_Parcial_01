01b4db4a3ebee8d11fe3a561f36c42f6c5711a72
AnderSeven <andersonmanoloalvaradoalvarez@gmail.com>
2025-07-28 18:29:01 -0600

    Empezamos las clases por la clase principal

---
+++ Evaluacion_Parcial_01.py
@@ -0,0 +1,9 @@
+empleados = {}
+class Empleado:
+    def __init__(self, codigo, nombre, departamento, anios):
+        self.codigo = codigo
+        self.nombre = nombre
+        self.departamento = departamento
+        self.anios = anios
+
+class
