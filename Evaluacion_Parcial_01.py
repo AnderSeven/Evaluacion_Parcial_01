9198db6c1386ee5bf908f0ad05dcd2fd6b4234cb
AnderSeven <andersonmanoloalvaradoalvarez@gmail.com>
2025-07-28 18:32:42 -0600

    avances en las clases

--- Evaluacion_Parcial_01.py
+++ Evaluacion_Parcial_01.py
@@ -1,9 +1,19 @@
 empleados = {}
 class Empleado:
-    def __init__(self, codigo, nombre, departamento, anios):
+    def __init__(self, codigo, nombre, departamento, antiguedad):
         self.codigo = codigo
         self.nombre = nombre
         self.departamento = departamento
-        self.anios = anios
+        self.antiguedad = antiguedad
 
-class
+class Evaluacion:
+    def __init__(self, puntualidad, trabajo_equipo, productividad):
+        self.puntualidad = puntualidad
+        self.trabajo_equipo = trabajo_equipo
+        self.pruductividad = productividad
+
+class InformacionContacto:
+    def __init__(self, telefono, correo):
+        self.telefono = telefono
+        self.correo = correo
+
